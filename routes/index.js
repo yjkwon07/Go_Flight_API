f927bc70843597efee0b17c6f673767177117d45
YoungJun <dudwns9073@nate.com>
2019-08-29 18:55:46 +0900

    clientsecret와 uuid [UPDATE]

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const uuidv4 = require('uuid/v4');
 const router = express.Router();
 const { User , Post, Domain } = require('../models');
 
@@ -14,6 +14,25 @@ router.get('/', (req, res, next) => {
                 loginError: req.flash('loginError'),
                 domains: user && user.domains,
             })
+        })
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        })
+});
+
+/*
+    도메인 주소는 프론트 요청 시, 클라이언트 시크릿은 서버 요청 시 검사한다.
+*/
+router.post('/domain', (req, res, next) => {
+    Domain.create({
+        userId: req.user.id,
+        host: req.body.host,
+        type: req.body.type,
+        clientSecret: uuidv4(),
+    })
+        .then(()=>{
+            res.redirect('/');
         })
         .catch((error) => {
             console.error(error);
