7cd6fc9df1c05f6b8447b0e3d23e27326f10089f
YoungJun <dudwns9073@nate.com>
2019-08-30 17:09:06 +0900

    post, hashtag api 응답 api [UPDATE]

--- routes/v1.js
+++ routes/v1.js
@@ -1,12 +1,12 @@
-const express=require('express');
-const jwt=require('jsonwebtoken');
+const express = require('express');
+const jwt = require('jsonwebtoken');
 
-const { verifyToken }=require('./middlewares');
-const { Domain, User, }=require('../models');
+const { verifyToken } = require('./middlewares');
+const { Domain, User, Post, Hashtag} = require('../models');
 
 const router = express.Router();
 
-router.post('/token', async (req, res, next)=>{
+router.post('/token', async (req, res) => {
     const { clientSecret } = req.body;
     /*
         API 서버의 응답 형식은 하나로 통일해주는 게 좋다. 
@@ -21,7 +21,7 @@ router.post('/token', async (req, res, next)=>{
                 attribute: ['nick', 'id'],
             }]
         });
-        if(!domain) {
+        if (!domain) {
             return res.status(401).json({
                 code: 401,
                 message: "등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요."
@@ -31,11 +31,11 @@ router.post('/token', async (req, res, next)=>{
             id: domain.user.id,
             nick: domain.user.nick,
         }, process.env.JWT_SECRET, {
-            // 유효 시간
-            expiresIn: '1m',
-            // 발급자 
-            issuer: 'GoFlight',
-        });
+                // 유효 시간
+                expiresIn: '1m',
+                // 발급자 
+                issuer: 'GoFlight',
+            });
         return res.json({
             code: 200,
             message: '토큰이 발급되었습니다.',
@@ -47,11 +47,52 @@ router.post('/token', async (req, res, next)=>{
             code: 500,
             message: error.message
         });
-1    }
+        
+    }
 });
 
-router.get('/test', verifyToken, (req , res) => {
+router.get('/test', verifyToken, (req, res) => {
     res.json(req.decoded);
 });
 
+router.get('/posts/my', verifyToken, (req, res) => {
+    Post.findAll({ where: { userId: req.decoded.id } })
+        .then((posts) => {
+            console.log(posts);
+            res.json({
+                code: 200,
+                payload: posts,
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        });
+});
+
+router.get('/posts/hashtag/:title', verifyToken, async(req, res)=>{
+    try {
+        const hashtag = await Hashtag.findOne({where : {title : req.params.title}});
+        if(!hashtag){
+            return res.status(404).json({
+                code: 404,
+                message: '검색 결과가 없습니다.',
+            });
+        }
+        const posts = await hashtag.getPosts();
+        return res.json({
+            code: 200,
+            payload: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        });
+    }
+})
 module.exports = router;
