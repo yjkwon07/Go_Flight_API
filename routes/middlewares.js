67d408e13439013daff23bf43e4fae4aa0e442a2
YoungJun <dudwns9073@nate.com>
2019-09-19 17:53:55 +0900

    불필요한 주석 제거

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -18,19 +18,15 @@ exports.isNotLoggedIn = (req, res, next) => {
 
 exports.verifyToken = (req, res, next) => {
     try {
-        // jwt.verify(토큰, JWT시크릿)
-        // 검증 실패 시 error
        req.decoded=jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
        next();
     } catch (error) {
-        // 토큰의 유효기간을 짧게 주고 자주 재발급 받는 방식으로 취한다.
         if (error.name === 'TokenExpiredError') {
             return res.status(419).json({
                 code: 419,
                 message: '토큰이 만료되었습니다.'
             });
         }
-        // 위조 토큰 발견시
         return res.status(401).json({
             code: 401,
             message: '유효하지 않은 토큰입니다.'
