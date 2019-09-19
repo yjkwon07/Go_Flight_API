a6109feda234707c46636b8132d1e0111a456672
YoungJun <dudwns9073@nate.com>
2019-09-20 01:02:03 +0900

    API 사용량 제한 추가

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) { // 로그인 여부
@@ -18,8 +19,8 @@ exports.isNotLoggedIn = (req, res, next) => {
 
 exports.verifyToken = (req, res, next) => {
     try {
-       req.decoded=jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-       next();
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        next();
     } catch (error) {
         if (error.name === 'TokenExpiredError') {
             return res.status(419).json({
@@ -32,4 +33,23 @@ exports.verifyToken = (req, res, next) => {
             message: '유효하지 않은 토큰입니다.'
         });
     }
+};
+
+exports.apiLimiter = new RateLimit({
+    windowMs: 60 * 1000,
+    max: 1,
+    delayMs: 0,
+    handler(_req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode, //429
+            message: '1분에 한 번만 요청할 수 있습니다',
+        });
+    },
+});
+
+exports.deprecated = (_req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: "새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.",
+    });
 };
