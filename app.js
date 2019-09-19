9753401c452d3711a5e35eeaeca254f06f5246cd
YoungJun <dudwns9073@nate.com>
2019-09-19 18:50:43 +0900

    css 경로 수정 [UPDATE]

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 const passportConfig = require('./passport');
 const { sequelize } = require('./models');
+
 const autRouter = require('./routes/auth');
 const indexRouter = require('./routes');
 const v1 = require('./routes/v1');
@@ -15,6 +16,7 @@ const v1 = require('./routes/v1');
 require('dotenv').config();
 passportConfig(passport);
 sequelize.sync(); 
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -22,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 8002);
 
 app.use(morgan('dev'));
-app.use('/static',express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static(path.join(__dirname, 'public/css')));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -43,15 +45,15 @@ app.use('/v1', v1);
 app.use('/auth', autRouter);
 app.use('/', indexRouter);
 
-app.use((req, res, next)=> {
-    const err= new Error("NotFound");
+app.use((_req, _res, next)=> {
+    const err=new Error("NotFound");
     err.status=404;
     next(err);
 });
 
 app.use((err,req, res)=>{
-    req.locals.message = err.message;
-    req.locals.error = req.app.get('env') === 'development'? err: {};
+    req.locals.message=err.message;
+    req.locals.error=req.app.get('env') === 'development'? err: {};
     res.status(err.status || 505);
     app.render('error');
 });
