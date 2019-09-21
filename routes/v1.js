2b5b50cb58eeaf8fa36500c27bd1ac7efc1195b1
YoungJun <dudwns9073@nate.com>
2019-09-21 13:43:43 +0900

    토크 create 추가

--- routes/v1.js
+++ routes/v1.js
@@ -4,7 +4,52 @@ const { Post, Hashtag, User } = require('../models');
 
 const router = express.Router();
 router.use(deprecated);
+
+router.post('/create', async (req, res) => {
+    const { frontSecret } = req.body;
+    try {
+        const domain = await Domain.findOne({
+            where: { frontSecret },
+            include: [{
+                model: User,
+                attribute: ['id', 'nick'],
+                as: 'User',
+            }],
+        });
+        if (!domain) {
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인 입니다. 먼저 도메인을 등록하세요.',
+            });
+        }
+        else {
+            const token = await jwt.sign({
+                id: domain.User.id,
+                nick: domain.User.nick,
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1m',
+                issuer: 'GoFlight',
+            });
+            return res.json({
+                code: 200,
+                message: "토큰이 발급되었습니다.",
+                token,
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.json({
+            code: 500,
+            message: error.message,
+        });
+    }
+});
+
 router.use(verifyToken);
+
+router.get('/test', (req, res) => {
+    res.json(req.decoded);
+});
 
 router.get('/posts/id', async (req, res) => {
     Post.findAll({ where: { userId: req.decoded.id } })
